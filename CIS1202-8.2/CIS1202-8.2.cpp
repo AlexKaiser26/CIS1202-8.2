2a88a43561a6a7657dbc6f208350fab4b5beddc0
ATK <54485972+ATK422@users.noreply.github.com>
2023-11-26 19:04:07 -0500

    Added a method to offset characters Wrote a driver in main to test the new method

--- CIS1202-8.2/CIS1202-8.2.cpp
+++ CIS1202-8.2/CIS1202-8.2.cpp
@@ -2,3 +2,51 @@
 //CIS 1202 801
 //November 26, 2023
 
+#include <cctype>
+#include <iostream>
+
+
+char character(const char start, int offset)
+{
+	if (!std::isalpha(start))
+	{
+		std::string e = "invalidCharacterException";
+		throw e;
+	}
+
+	const char target = start + offset;
+	if (std::isalpha(target) && std::isupper(target) == std::isupper(start))
+	{
+		return target;
+	}
+
+	std::string e = "invalidRangeException";
+	throw e;
+}
+
+int main()
+{
+	character('a', 1);
+	std::cout << "Offset a by 1 passed" << std::endl;
+
+	try
+	{
+		character('a', -1);
+	}
+	catch (std::string exception)
+	{
+		std::cout << "Offsetting a by -1 failed: " << exception << std::endl;
+	}
+
+	character('Z', -1);
+	std::cout << "Offset Z by -1 passed" << std::endl;
+
+	try
+	{
+		character('?', 5);
+	}
+	catch (std::string exception)
+	{
+		std::cout << "Offsetting ? by 5 failed: " << exception << std::endl;
+	}
+}
